30c2e02eab96635d354dbff8db8159094f24bebb
Jarkko Bamberg <jarkko.bamberg@gmail.com>
2020-12-08 22:37:47 +0200

    uibuild

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
+app.use(express.static('build'));
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
---
+++ build/precache-manifest.75c1144e616bb10f166235958fe3861d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823ceb9f20ffc41f7a5aecb2fc41beba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75dec53d8e1305435008",
+    "url": "/static/js/2.405ff848.chunk.js"
+  },
+  {
+    "revision": "89373738761ad72379e7095490ffffc2",
+    "url": "/static/js/2.405ff848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be803b40c55ba24df9c",
+    "url": "/static/js/main.1b0cabb4.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
